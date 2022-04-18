a538cee7390d3bc560b670713621d2faf3616a84
sangsook <sisily11@naver.com>
2022-04-18 14:17:27 +0900

    11

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/sing_in/bindings/sing_in_binding.dart';
+import '../modules/sing_in/views/sing_in_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SING_IN,
+      page: () => SingInView(),
+      binding: SingInBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const SING_IN = _Paths.SING_IN;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const SING_IN = '/sing-in';
 }
